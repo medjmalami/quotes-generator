f4e8d4e2d177bee63d5754264cc92d2049870b61
unknown <jmalmohamedamine1@gmail.com>
2024-09-05 22:36:54 +0200

    first commit

--- quote-generator/src/index.js
+++ quote-generator/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
